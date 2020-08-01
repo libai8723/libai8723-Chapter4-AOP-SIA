216b6ce09560be7b8c55b40e7562e9542a822dfb
libai8723 <libai8723@qq.com>
2020-08-01 09:28:35 +0800

    add @PointCut Annotation
    and replace all the longer PointCut Expression with shorter function call name

--- src/main/java/io/github/libai8723/Audience.java
+++ src/main/java/io/github/libai8723/Audience.java
@@ -1,27 +1,41 @@
 package io.github.libai8723;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 
 @Aspect
 public class Audience {
-    @Before("execution(** io.github.libai.Performance.perform(..))")
+
+    /**
+     * The value given to the @Pointcut annotation is a pointcut expression, just like the ones you
+     * used previously with the advice annotations. By annotating performance() with @Pointcut in this way,
+     * you essentially extend the pointcut expression language so that
+     * you can use performance() in your pointcut expressions anywhere you’d otherwise
+     * use the longer expression. As you can see, you replace the longer expression in all the
+     * advice annotations with performance().
+     *
+     * The body of the performance() method is irrelevant and, in fact, should be
+     * empty. The method itself is just a marker, giving the @Pointcut annotation something
+     * to attach itself to
+     */
+    @Pointcut("execution(** io.github.libai8723.Performance.perform(..))")
+    public void performance() {}
+
+    // the same meaning with the longer expression
+    @Before("performance()")
     public void silenceCellPhones() {
         System.out.println("Silencing cell phones");
     }
 
-    @Before("execution(** io.github.libai.Performance.perform(..))")
+    @Before("performance()")
     public void takeSeats() {
         System.out.println("Taking seats");
     }
 
-    @AfterReturning("execution(** io.github.libai.Performance.perform(..))")
+    @AfterReturning("performance()")
     public void applause() {
         System.out.println("CLAP CLAP CLAP!!!");
     }
-    @AfterThrowing("execution(** io.github.libai.Performance.perform(..))")
+    @AfterThrowing("performance()")
     public void demandRefund() {
         System.out.println("Demanding a refund");
     }
